e2f5f3a66a9ac2161f35be6f31a27371d4d79a9d
martinhernandezruiz <martinbaez633001@gmail.com>
2022-04-18 09:15:26 -0500

    Initial commit

---
+++ app/vistas/main/foot.php
@@ -0,0 +1,19 @@
+<script type="text/javascript" src="sys/libs/js/bootstrap.js"></script>
+<div id="titulo-principal" class="titulo1"> 
+    
+    <footer class="text-center">
+    <h1 id="titulo-principal" class="titulo1">Redes sociales</h1>
+    <a href= "https://github.com/martinhernandezruiz" > 
+         <img src="sys/libs/images/GitHub-Logo.png" width="8%">    
+    </a> 
+    <a href= "https://www.youtube.com/channel/UCQyqLKl1LFFfEnjfWjdrCqw" > 
+         <img src="sys/libs/images/youtube.png" width="6%">         
+    </a>
+    <a href= "https://www.facebook.com/martin.hernandezru"> 
+         <img src="sys/libs/images/face1.png" width="7%">        
+    </a>
+
+    </footer>
+</div>
+</body>  
+</html>
